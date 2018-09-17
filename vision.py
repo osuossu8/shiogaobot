05683d6b2cac5a5c28736ab95e78d34350739de9
osuossu <osuosuossu18@gmail.com>
2018-09-17 23:12:28 +0900

    model_ok

--- vision.py
+++ vision.py
@@ -74,8 +74,8 @@ def detect_who(img):
     print(img)
     face=""
     print("** 5 **")
-    if not model:        
-        model = load_model('./shiogao_model2.h5', compile=False)
+    model = null   
+    model = load_model('./shiogao_model2.h5', compile=False)
     print(model.summary())
     print("** 6 **")
     print(model.predict(img))
